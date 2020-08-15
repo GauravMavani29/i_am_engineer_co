430affc3d02c8088a4fe0796aeceb020a12fbf2a
gauravmavani <gauravmavani29@gmail.com>
2020-08-15 09:50:28 +0530

    First commit

---
+++ PHP/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LogIn</title>
+<link rel="stylesheet" type="text/css" href="../assets/css/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@1,300&display=swap" rel="stylesheet">
+</head>
+<body>
+		<div class="lg_bg">
+				<div class="Maindiv">
+						<form method="get" accept-charset="utf-8" class="log">
+							<div class="login">LOGIN</div>
+							<div>
+								 <input type="text" name="UserName" placeholder="UserName">
+							</div>
+							<div>
+								 <input type="text" name="Password" placeholder="Password">
+							</div>	
+							<div>
+								<input type="submit" name="Login" value="LogIn">
+							</div>
+							<div class="cna">
+								<a href="">create new account</a>
+							</div>
+						</form>
+				</div>
+		</div>	
+</body>
+</html>
